72c8823d4ba26fcf589c716147d09f75342720f9
jackson2516484 <wssugs@gmail.com>
2025-07-15 21:22:13 +0800

    feat: finally add fetch interceptor

--- index.tsx
+++ index.tsx
@@ -1,3 +1,31 @@
+// --- 网络请求拦截代码开始 ---
+const GOOGLE_API_HOSTNAME = 'generativelanguage.googleapis.com';
+const PROXY_PATH = '/api';
+
+const originalFetch = window.fetch;
+window.fetch = (input, init) => {
+  let urlString = '';
+  if (typeof input === 'string') {
+    urlString = input;
+  } else if (input instanceof URL) {
+    urlString = input.href;
+  } else {
+    urlString = input.url;
+  }
+
+  if (urlString.includes(GOOGLE_API_HOSTNAME)) {
+    const originalUrl = new URL(urlString);
+    // 将请求重定向到我们自己的代理路径
+    const newUrl = `${PROXY_PATH}${originalUrl.pathname}${originalUrl.search}`;
+    console.log(`Intercepting and redirecting request to: ${newUrl}`);
+    return originalFetch(newUrl, init);
+  }
+  
+  return originalFetch(input, init);
+};
+// --- 网络请求拦截代码结束 ---
+
+// --- 您原来的代码开始 ---
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -13,3 +41,4 @@ root.render(
     <App />
   </React.StrictMode>
 );
+// --- 您原来的代码结束 ---
